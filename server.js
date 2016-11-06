c701647803c2ec7d5e9b4a1a88cbc629006b2b75
Shannon Green <shannongreen@Shannons-MacBook-Pro.local>
2016-11-05 20:14:40 -0500

    building survey and changed port info

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var path = require('path');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var appPORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -25,6 +25,6 @@ require('./app/routing/html-routes.js')(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function () {
-	console.log('App listening on PORT ' + PORT);
+app.listen(appPORT, function () {
+	console.log('App listening on PORT ' + appPORT);
 });
